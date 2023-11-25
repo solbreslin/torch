1b0eb731a921a0d97a110091bb65fd90e27b4a52
solbreslin <hi@solbreslin.com>
2023-11-25 07:36:28 +0000

    Update eslint settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './routes/Home/Home';
 import AboutPage from './routes/About/About';
 
-import { Routes, Route } from 'react-router-dom';
 import { Layout } from './shared/Layout/Layout';
 
- const App = () => {
+const App = () => {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-         
         </Route>
       </Routes>
     </main>
   );
 };
 
-export default App
+export default App;
