423fd5cccb5a588559d8bfc924e206232677de80
solbreslin <hi@solbreslin.com>
2023-11-26 17:34:41 +0000

    Update about page

--- src/routes/About/About.styled.ts
+++ src/routes/About/About.styled.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  padding: 4rem 0;
+`;
--- src/routes/About/Vision/Vision.styled.ts
+++ src/routes/About/Vision/Vision.styled.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Root = styled.section``;
+export const Root = styled.section`
+  max-width: 66ch;
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.675rem;
+  }
+`;
