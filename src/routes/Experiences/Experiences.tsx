776d7881284be897468100defd20bee41bbfc1c4
Sol Breslin <sol@vitruehealth.com>
2024-02-03 09:58:20 +0000

    Add hidden input

--- src/routes/Experiences/Experiences.tsx
+++ src/routes/Experiences/Experiences.tsx
@@ -6,27 +6,18 @@ const ExperiencesPage = () => {
     <Root>
       <div>
         <p>
-          We are seeking to understand patterns and themes in workplace
-          experiences based on gender, ethnicity, age, location etc. All
-          questions are voluntary, but the more we can learn the better we can
-          understand how the experience of the world of work is different for
-          one person to the next. We want to gather our first X,000 responses in
-          order to validate that Torch can be a unique source of data to shine a
-          light on real life experiences in the work place. If you think this
-          mission is worthwhile, we'd really appreciate your help and taking a
-          few minutes to share your experiences. Please note - Any trends /
-          findings from our analysis will always be presented in a manner that
-          ensures the anonymity of all participants. No personal identifiers
-          (such as names, contact details, or specific addresses) will ever be
-          shared publicly.
+          No personal identifiers (such as names, contact details, or specific
+          addresses) will ever be shared publicly.
         </p>
       </div>
       <form
-        name="contact"
+        name="experiences"
         method="POST"
         data-netlify="true"
         data-netlify-recaptcha="true"
       >
+        <input type="hidden" name="form-name" value="experiences" />
+
         <div>
           <label>
             <span>Company name</span>
